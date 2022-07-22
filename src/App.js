3a72334ac0c5db2377c61c33f99e8aec35892012
bouldersky <skyarnold1@me.com>
2022-07-21 21:22:46 -0600

    single-line input works!

--- src/App.js
+++ src/App.js
@@ -8,12 +8,17 @@ class App extends Component {
         super(props);
 
         this.state = {pgn: "1. d4 d5 2. e3 (2. f4 g5 3. fxg5 f6 4. gxf6 exf6) 2... e6 3. f4 *"};
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({pgn: event.target.value})
     }
 
     render() {
         return (<>
-            <div>foo</div>
             <div className="app-container">
+                <input type="text" onChange={this.handleChange} value={this.state.pgn}/>
                 <PgnTree rootNode={MoveNode.fromPgn(this.state.pgn)}/>
             </div>
         </>);
