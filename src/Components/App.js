241d6f33c623482683519680e74e30ba1512ab39
bouldersky <skyarnold1@me.com>
2022-08-25 17:58:21 -0600

    remove old UI

---
+++ src/Components/App.js
@@ -0,0 +1,7 @@
+import './App.css';
+import {Component} from "react";
+
+class App extends Component {
+}
+
+export default App;
--- src/services/definitions/StudyProviderService.ts
+++
@@ -1,4 +0,0 @@
-export interface StudyProviderService {
-    getLastGamePgn(username: string): Promise<string>;
-    getAllGamePgns(username: string): Promise<Array<string>>;
-}
