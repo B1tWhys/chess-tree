b396ce95f6a5a591240f5774cc5554d9873889f4
bouldersky <skyarnold1@me.com>
2022-08-25 19:38:43 -0600

    layout is working

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,12 +1,37 @@
 import './App.css';
 import {Component} from "react";
 
-class App extends Component {
 
+class AnalysisSelector extends Component {
     render() {
-        return (<div>
-            foo
-        </div>)
+        return <div className={"analysis-selector"}>
+        </div>
+    }
+}
+
+class AnalysisTree extends Component {
+    render() {
+        return <div className={"analysis-tree-container"}>
+        </div>
+    }
+}
+
+class ChessBoard extends Component {
+    render() {
+        return <div className={"chess-board"}>
+        </div>
+    }
+}
+
+class App extends Component {
+    render() {
+        return (
+            <div id={"app-container"}>
+                <AnalysisSelector/>
+                <AnalysisTree/>
+                <ChessBoard/>
+            </div>
+        )
     }
 }
 
