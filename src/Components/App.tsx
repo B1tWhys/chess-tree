fb7136b902057a2794123ad0c8ed350db19a60c9
bouldersky <skyarnold1@me.com>
2022-08-25 18:05:46 -0600

    App is typescript

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -2,6 +2,12 @@ import './App.css';
 import {Component} from "react";
 
 class App extends Component {
+
+    render() {
+        return (<div>
+            foo
+        </div>)
+    }
 }
 
 export default App;
