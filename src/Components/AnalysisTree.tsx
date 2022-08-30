ee06d5ea04eb1d67b9544c97600fc2ada0451110
bouldersky <skyarnold1@me.com>
2022-08-29 19:59:42 -0600

    rendering natively (ish) works great now with pan/zoom

--- src/Components/AnalysisTree.tsx
+++ src/Components/AnalysisTree.tsx
@@ -1,16 +1,21 @@
 import {Box, useTheme} from "@mui/material";
-import {useEffect, useRef, useState} from "react";
-import chessTreePlaceholder from "../assets/ChessTreePlaceholder.png";
+import {useEffect, useMemo, useRef, useState} from "react";
 import GameTree from "../types/GameTree";
-import {select, hierarchy, tree, linkVertical} from "d3";
-import {MoveNode} from "../types/MoveNode";
+import {hierarchy, linkVertical, tree} from "d3";
+import "@fontsource/roboto-mono/600.css"
+import panzoom from "panzoom";
 
 interface Props {
     gameTree: GameTree
 }
 
+const genLinkPath = linkVertical()
+    .x(d => d.x)
+    .y(d => d.y);
+
 export function AnalysisTree({gameTree}: Props) {
     const containerRef = useRef()
+    const treeRef = useRef()
     const [width, setWidth] = useState(0);
     const [height, setHeight] = useState(0);
 
@@ -21,7 +26,6 @@ export function AnalysisTree({gameTree}: Props) {
             if (!entries.length) return
             const rect = entries[0].contentRect
 
-            console.log(`width: ${rect.width} height: ${rect.height}`)
             setWidth(rect.width)
             setHeight(rect.height)
         })
@@ -31,42 +35,52 @@ export function AnalysisTree({gameTree}: Props) {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    const svgRef = useRef()
-    const theme = useTheme();
-
     useEffect(() => {
-        const svgElement = select(svgRef.current)
-        const root = hierarchy(gameTree.firstMoves[0])
-        root.sort()
-        const [dx, dy] = [80, 120];
-        tree().nodeSize([dx, dy])(root)
+        panzoom(treeRef.current)
+    }, [])
 
-        svgElement.select('g').remove()
+    const theme = useTheme();
+    const nodeTree = useMemo(() => {
+        const treeRoot = hierarchy(gameTree.firstMoves[0])
+        treeRoot.sort()
+        const [dx, dy] = [100, 155];
+        tree().nodeSize([dx, dy])(treeRoot)
+        return treeRoot;
+    }, [gameTree])
 
-        console.log("d3 appending...")
-        const treeGroup = svgElement.append('g')
-            .attr('transform', 'translate(200, 50)')
-        treeGroup.selectAll('path')
-            .data(root.links())
-            .join('path')
-            .attr('d', linkVertical()
-                .x(d => d.x)
-                .y(d => d.y))
-            .attr('fill', 'none')
-            .attr('stroke', 'gray')
-            .attr('stroke-width', '3')
-        treeGroup.selectAll('circle')
-            .data(root.descendants())
-            .join('circle')
-                .attr('r', 30)
-                .attr('cx', d => d.x)
-                .attr('cy', d => d.y)
-                .attr('fill', d => d.data.isWhiteTurn ? theme.squares.light : theme.squares.dark)
-                .attr('stroke', d => d.data.isWhiteTurn ? theme.squares.dark : theme.squares.light)
-                .attr('stroke-width', 3)
-    }, [gameTree, width, height, theme.squares])
+    function generateLinks() {
+        return nodeTree.links().map((n, i) => {
+            return <path key={i} d={genLinkPath(n)}
+                         fill={"none"}
+                         stroke={"gray"}
+                         strokeWidth={3}/>
+        });
+    }
+
+    function generateNodes() {
+        return nodeTree.descendants().map((n, i) => {
+            const isWhiteTurn = n.data.isWhiteTurn
+            return <g key={i} transform={`translate(${n.x}, ${n.y})`}>
+                <circle r={40}
+                        fill={isWhiteTurn ? theme.squares.light : theme.squares.dark}
+                        stroke={isWhiteTurn ? theme.squares.dark : theme.squares.light}
+                        strokeWidth={3}/>
+                <text textAnchor="middle"
+                      fontFamily={"Roboto Mono, monospace"}
+                      fontWeight={600}
+                      fontSize={35}
+                      fill={isWhiteTurn ? theme.squares.dark : theme.squares.light}
+                      dy=".35em">{n.data.name}</text>
+            </g>
+        });
+    }
 
     return <Box sx={{flexGrow: 1}} ref={containerRef}>
-        <svg ref={svgRef} width={width} height={height}/>
+        <svg width={width} height={height} ref={treeRef}>
+            <g transform={`translate(${width / 2}, 50)`}>
+                {generateLinks()}
+                {generateNodes()}
+            </g>
+        </svg>
     </Box>
 }
