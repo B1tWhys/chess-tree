cf43214cc74fdefcf252ee616e27726b1db080ee
skyler arnold <skyarnold1@me.com>
2022-12-03 17:32:17 -0700

    basic layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col h-full">
+	<div class="text-center bg-gray-800 text-gray-50">Menu bar</div>
+	<div class="relative flex-grow">
+		<div class="absolute p-3 right-0 top-0 bg-gray-400 w-52 aspect-square text-white">
+			Chessboard
+		</div>
+		<div class="bg-neutral-900 h-full" />
+	</div>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'black-move': '#769656',
+				'white-move': '#eeeed2'
+			}
+		}
 	},
 	plugins: []
 };
