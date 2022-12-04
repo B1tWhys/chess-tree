7717d5558a4e83c1256fdd3818e740c5a00174e5
skyler arnold <skyarnold1@me.com>
2022-12-03 19:09:32 -0700

    chess board is displayed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
+<script lang="ts">
+	import Chessboard from './chessboard.svelte';
+</script>
+
 <div class="flex flex-col h-full">
 	<div class="text-center bg-gray-800 text-gray-50">Menu bar</div>
 	<div class="relative flex-grow">
-		<div class="absolute p-3 right-0 top-0 bg-gray-400 w-52 aspect-square text-white">
-			Chessboard
-		</div>
+        <Chessboard />
 		<div class="bg-neutral-900 h-full" />
 	</div>
 </div>
---
+++ src/routes/chessboard.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { Chessground } from 'chessground/chessground';
+	import 'chessground/assets/chessground.base.css';
+	import 'chessground/assets/chessground.brown.css';
+	import 'chessground/assets/chessground.cburnett.css';
+	import './chessboard.css';
+	import { onMount } from 'svelte';
+
+	let cg;
+	let board: HTMLElement;
+	let config = {};
+
+	onMount(() => {
+		cg = Chessground(board, config);
+	});
+    
+</script>
+
+<div class="absolute box-border right-0 border-l-4 border-b-4 border-white rounded-bl-md">
+	<div bind:this={board} class="w-80 h-80" />
+</div>
+
+<style>
+</style>
