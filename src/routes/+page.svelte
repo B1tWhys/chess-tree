3f97d1af8531e40eeb207943714af3f9cc85cbab
skyler arnold <skyarnold1@me.com>
2022-12-04 13:34:09 -0700

    can update board state

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
 	import Chessboard from './chessboard.svelte';
+
+	let fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+
+	function changeFen() {
+		console.log('updating fen');
+		fen = 'rnbqkbnr/pppppppp/8/8/8/P7/1PPPPPPP/RNBQKBNR w KQkq - 0 1';
+	}
 </script>
 
 <div class="flex flex-col h-full">
 	<div class="text-center bg-gray-800 text-gray-50">Menu bar</div>
 	<div class="relative flex-grow">
-        <Chessboard />
-		<div class="bg-neutral-900 h-full" />
+		<Chessboard {fen} />
+		<div class="bg-neutral-900 h-full">
+			<button class="bg-white rounded-sm" on:click={changeFen}>Change fen</button>
+		</div>
 	</div>
 </div>
--- src/routes/chessboard.svelte
+++ src/routes/chessboard.svelte
@@ -1,19 +1,32 @@
 <script lang="ts">
 	import { Chessground } from 'chessground/chessground';
+	import type { FEN } from 'chessground/types';
+	import type { Api } from 'chessground/api';
+	import type { Config } from 'chessground/config';
 	import 'chessground/assets/chessground.base.css';
 	import 'chessground/assets/chessground.brown.css';
 	import 'chessground/assets/chessground.cburnett.css';
 	import './chessboard.css';
 	import { onMount } from 'svelte';
 
-	let cg;
+	export let fen: string;
+
+	let cg: Api;
 	let board: HTMLElement;
-	let config = {};
+
+	let config: Config = { fen: fen, viewOnly: true };
 
 	onMount(() => {
 		cg = Chessground(board, config);
 	});
-    
+
+	$: {
+		console.log('fen update triggered config change');
+		config.fen = fen;
+		if (cg) {
+			cg.set(config);
+		}
+	}
 </script>
 
 <div class="absolute box-border right-0 border-l-4 border-b-4 border-white rounded-bl-md">
