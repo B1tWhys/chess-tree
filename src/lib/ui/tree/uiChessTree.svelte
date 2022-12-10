b5fc948fc05b6e80d30722c79b2f35fd9640a1f6
skyler arnold <skyarnold1@me.com>
2022-12-09 17:24:46 -0700

    d3 is drawing nodes and panning and zooming them!

--- src/lib/ui/tree/uiChessTree.svelte
+++ src/lib/ui/tree/uiChessTree.svelte
@@ -1,25 +1,41 @@
 <script lang="ts">
-	import { hierarchy, select, tree, type HierarchyNode } from 'd3';
+	import { hierarchy, select, tree, zoom, type ZoomBehavior } from 'd3';
 	import type GameTree from '$lib/core/GameTree';
 	import { onMount } from 'svelte';
 
 	export let gameTree: GameTree;
 
-	let el: HTMLElement, width: number, height: number;
+	let treeSvg: SVGElement;
+	let panZoomContainer: SVGElement;
+
+	let width: number, height: number;
 
 	$: root = hierarchy(gameTree.rootMoves[0]);
-	$: treeGen = tree().size([width, height]);
-	$: treeLayout = treeGen(root);
+
+	function handleZoom(e: ZoomBehavior<SVGElement, unknown>) {
+		select(panZoomContainer).attr('transform', e.transform);
+	}
 
 	onMount(() => {
-		select(el)
+		const treeGen = tree().size([width, height]);
+		const treeLayout = treeGen(root);
+		select(panZoomContainer)
+			.selectAll('circle')
 			.data(treeLayout)
 			.enter()
 			.append('circle')
 			.attr('cx', (d) => d.x)
-			.attr('cy', (d) => d.y);
+			.attr('cy', (d) => d.y)
+			.attr('r', 50)
+			.attr('class', 'fill-white-move stroke-black-move stroke-7');
+
+		let z = zoom().on('zoom', handleZoom);
+		select(treeSvg).call(z);
 	});
 </script>
 
-<div class="h-full" bind:this={el} bind:clientHeight={height} bind:clientWidth={width} >
+<div class="h-full" bind:clientHeight={height} bind:clientWidth={width}>
+	<svg bind:this={treeSvg} {width} {height}>
+		<g bind:this={panZoomContainer} />
+	</svg>
 </div>
