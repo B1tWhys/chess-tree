f496fd1ff6d8b400274020e6fd1cb6bdea23547b
skyler arnold <skyarnold1@me.com>
2022-12-13 17:32:47 -0700

    first pass at styling load games form

--- src/lib/ui/controlPannel/controlPannel.svelte
+++ src/lib/ui/controlPannel/controlPannel.svelte
@@ -2,4 +2,7 @@
 	import LoadData from './loadData.svelte';
 </script>
 
-<LoadData />
+<div class="h-full w-80 p-2 bg-gray-800 text-gray-50">
+	<LoadData />
+	<div class="h-0.5 bg-gray-300 rounded-full" />
+</div>
--- src/lib/ui/controlPannel/loadData.svelte
+++ src/lib/ui/controlPannel/loadData.svelte
@@ -15,14 +15,25 @@
 	}
 </script>
 
-<div class="h-full w-80 p-2 bg-gray-800 text-gray-50">
-	<div>
-		<form on:submit={onSubmit}>
-			<div>Load games</div>
-			<label for="username">Chess.com username</label>
-			<input type="text" name="username" class="text-black" bind:value={username} />
-			<button type="submit">Go!</button>
-		</form>
-	</div>
-	<div class="h-0.5 bg-gray-300 my-2 rounded-full" />
+<div>
+	<form on:submit={onSubmit}>
+		<div class="font-bold text-xl mb-3">Load games</div>
+		<div class="flex">
+			<label for="chessdotcom-username">Chess.com</label>
+			<input
+				type="text"
+				name="chessdotcom-username"
+				class="text-black ml-2 pl-1"
+				bind:value={username}
+				placeholder="username"
+			/>
+		</div>
+		<button
+			class="bg-black-move text-white-move my-3 rounded-sm py-0.5 px-2 font-semibold"
+			type="submit"
+		>
+			Go!
+		</button>
+	</form>
 </div>
+<div class="h-0.5 bg-gray-300 rounded-full" />
